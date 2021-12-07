abac633474fea7b74ba42aad4e2b394e6df7fc55
Yousuf Kalim <yousuf@falconit.com.pk>
2021-12-07 12:19:49 +0500

    Routes updated

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = require("express").Router();
 
 // Parent Routes
 router.use("/users", require("./users")); // All the user routes
+router.use("/auth", require("./auth")); // All the auth routes
 
 // Export
 module.exports = router;
