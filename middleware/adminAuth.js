da65b1b542e4fa526d0740594f0144e5bb99ca43
Yousuf Kalim <yousuf@falconit.com.pk>
2022-06-24 11:41:28 +0500

    added role check in checkAuth middleware

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -22,21 +22,24 @@ exports.checkAdminAuth = (req, res, next) => {
     // Decrypting token
     const token = header.split(" ")[1];
     const decoded = jwt.verify(token, tokenSecret);
+
+    if (!decoded.admin) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to access this resource",
+      });
+    }
+
     req.admin = decoded.admin;
     req.token = token;
 
     // If user authenticated
-    res.set(
-      "cache-control",
-      "no-cache, no-store, private, must-revalidate, post-check=0, pre-check=0"
-    );
+    res.set("cache-control", "no-cache, no-store, private, must-revalidate, post-check=0, pre-check=0");
 
     // Done
     next();
   } catch (err) {
     // If not
-    res
-      .status(403)
-      .json({ success: false, message: "Your session has been expired" });
+    res.status(403).json({ success: false, message: "Your session has been expired" });
   }
 };
--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -22,20 +22,23 @@ exports.checkAuth = (req, res, next) => {
     // Decrypting token
     const token = header.split(" ")[1];
     const decoded = jwt.verify(token, tokenSecret);
+
+    if (!decoded.user) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to access this resource",
+      });
+    }
+
     req.user = decoded.user;
 
     // If user authenticated
-    res.set(
-      "cache-control",
-      "no-cache, no-store, private, must-revalidate, post-check=0, pre-check=0"
-    );
+    res.set("cache-control", "no-cache, no-store, private, must-revalidate, post-check=0, pre-check=0");
 
     // Done
     next();
   } catch (err) {
     // If not
-    res
-      .status(403)
-      .json({ success: false, message: "Your session has been expired" });
+    res.status(403).json({ success: false, message: "Your session has been expired" });
   }
 };
