9b15f2f0c0a2581f79e673ff1d3668cbe7392997
Yousuf Kalim <yousuf@falconit.com.pk>
2022-10-05 17:36:35 +0500

    CORS policy updated

--- middleware/common.js
+++ middleware/common.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 // Common Middleware
 module.exports = (app) => {
-  app.use(cors({ origin: true })); // CORS (Cross Origin Policy) to restrict unknown requests
+  app.use(cors({ origin: process.env.ORIGIN })); // CORS (Cross Origin Policy) to restrict unknown requests
   app.use(logger('dev')); // Morgan - to log every request in console
   app.use(express.urlencoded({ extended: true })); // to get url encoded data from requests
   app.use(express.json()); // to send json data in response (It's mandatory in rest api's)
