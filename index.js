9360141aee4ef22cbcbdb535917e65b41c885f46
Yousuf Kalim <yousuf@falconit.com.pk>
2022-10-05 17:45:50 +0500

    Cluster mode enabled for prod env

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 const app = require('express')();
 const port = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV || 'dev';
 require('./database');
 
 // Middleware
@@ -13,8 +14,22 @@ require('./middleware/common')(app);
 // API Routes
 app.use('/api', require('./routes'));
 
-// Server
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running at port ${port} :)`);
-});
+// If we are in production mode, then we will use the cluster mode to create multiple processes
+if (NODE_ENV === 'prod') {
+  const cluster = require('cluster');
+  if (cluster.isMaster) {
+    require('os')
+      .cpus()
+      .forEach(() => cluster.fork());
+  } else {
+    app.listen(port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server is running in ${NODE_ENV} mode on port ` + port);
+    });
+  }
+} else {
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running in ${NODE_ENV} mode on port ` + port);
+  });
+}
